6a1756571ebc6bbf48c56fa22bd59d93f40e79a6
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-07 00:29:48 -0600

    temporal tag in reserva model

--- SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/model/Reserva.java
+++ SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/model/Reserva.java
@@ -11,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -31,7 +33,13 @@ public class Reserva {
 	@GeneratedValue(generator="system-uuid")
 	@GenericGenerator(name="systenm-uuid", strategy="uuid2")
 	private String idRes;
+	
+	// anotacion temporal de javax persistence
+	// de especificacion exacta de tipo de fecha para la db
+	@Temporal(TemporalType.DATE)
 	private Date fechaIngresoRes;
+	
+	@Temporal(TemporalType.DATE)
 	private Date fechaSalidaRes;
 	private int cantidadPersonasRes;
 	private String descripcionRes;
