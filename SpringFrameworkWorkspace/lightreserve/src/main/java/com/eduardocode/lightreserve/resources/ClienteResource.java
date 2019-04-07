1cc7a45a84afc8997c99a80cb4a9b9e61a945da6
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-07 03:57:04 -0500

    cliente class to improve view layer

---
+++ SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/resources/ClienteResource.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.eduardocode.lightreserve.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Resources package -> capa de vista de la aplicacion
+ * 
+ * Clase que representa el servicio web de cliente
+ * @author cheetos
+ *
+ */
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteResource {
+	//
+}
