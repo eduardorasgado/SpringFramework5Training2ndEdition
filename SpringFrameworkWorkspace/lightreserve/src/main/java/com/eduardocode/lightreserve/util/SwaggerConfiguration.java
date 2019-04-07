b99fa1fbec1ea9196e678498df7a1135d833e155
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-07 01:33:29 -0600

    swagger dependency included. Swagger configuration class in new util package.

---
+++ SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/util/SwaggerConfiguration.java
@@ -0,0 +1,10 @@
+package com.eduardocode.lightreserve.util;
+
+/**
+ * Clase que va a permitir habilitar todo el soporte de swagger en el proyecto
+ * @author cheetos
+ *
+ */
+public class SwaggerConfiguration {
+
+}
