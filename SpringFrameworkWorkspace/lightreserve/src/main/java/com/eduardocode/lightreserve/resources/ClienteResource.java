353835fd101f09ff3ca7ef65c60dfdbf972dd6b8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-08 01:30:59 -0500

    update cliente method in cliente resource. mappingClienteUtil method to help avoiding produce same cliente -clientevo classes mapping code

--- SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/resources/ClienteResource.java
+++ SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/resources/ClienteResource.java
@@ -5,7 +5,9 @@ package com.eduardocode.lightreserve.resources;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,15 +47,63 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> createCliente(@RequestBody ClienteVO clienteVo) {
 		// tomamos el cliente deseado y le mapeamos el cliente basado en un VO
 		Cliente cliente = new Cliente();
+		cliente = this.mappingClienteUtil(cliente, clienteVo);
+		
+		// retornamos la creacion del objeto + el estatus de creado
+		return new ResponseEntity<>(this.clienteService.create(cliente),
+				HttpStatus.CREATED);
+	}
+	
+	/**
+	 * Metodo que interactua con las peticiones externas para dar funcionalidad
+	 * a la actualizacion de un objeto de cliente existente en la base de datos
+	 * 
+	 * @param clienteVo
+	 * @return
+	 */
+	@PutMapping("/{identificacion}")
+	public ResponseEntity<Cliente> updateCliente(
+			@PathVariable("identificacion") String identificacion,
+			// aqui se va a mapear lo que venga del frontend, en clase pura,
+			// es decir sin anotaciones ni dependencias de spring
+			ClienteVO clienteVo) {
+		
+		Cliente cliente = this.clienteService.findByIdentificacion(identificacion);
+		
+		if(cliente == null) {
+			// en caso en el que no exista el usuario a actualizar
+			return new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND);
+		} else {
+			cliente = this.mappingClienteUtil(cliente, clienteVo);
+		}
+		
+		return new ResponseEntity<>(this.clienteService.update(cliente),
+				HttpStatus.OK);
+	}
+	
+	/**
+	 * Seccion de utilidades para el resource de cliente
+	 * @param cliente
+	 * @param clienteVo
+	 * @return
+	 */
+	
+	/**
+	 * Metodo de cliente resource que toma todos los valores de un objeto virtual
+	 * vo y los transfiere a un objeto de clase Cliente con anotacion Entity
+	 * @param cliente
+	 * @param clienteVo
+	 * @return
+	 */
+	private Cliente mappingClienteUtil(Cliente cliente, ClienteVO clienteVo) {
 		cliente.setNombreCli(clienteVo.getNombreCli());
 		cliente.setApellidoCli(clienteVo.getApellidoCli());
 		cliente.setIdentificacionCli(clienteVo.getIdentificacionCli());
 		cliente.setDireccionCli(clienteVo.getDireccionCli());
 		cliente.setTelefonoCli(clienteVo.getTelefonoCli());
 		cliente.setEmailCli(clienteVo.getEmailCli());
-		// retornamos la creacion del objeto + el estatus de creado
-		return new ResponseEntity<>(this.clienteService.create(cliente),
-				HttpStatus.CREATED);
+		
+		return cliente;
 	}
 	
 }
