063f4346a4701d3118fb0063d1346537d3ba8be3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-10 03:51:07 -0500

    login controller as part of controller package to be able to access login view. login method in login controller.

---
+++ SpringFrameworkWorkspace/lightreserve/src/main/java/com/eduardocode/lightreserve/controllers/LoginController.java
@@ -0,0 +1,44 @@
+package com.eduardocode.lightreserve.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Paquete Controladores: No forma parte de la aplicacion principal y solo se
+ * implementa por motivos de interfaz de usuario o empresa propietaria de esta API
+ * con el sistema de login y acceso a la documentacion de esta API
+ * 
+ * LoginController: Controla el acceso al login personalizo para el propietario
+ * de la API
+ * 
+ * @author cheetos
+ *
+ */
+
+@Controller
+@RequestMapping("/app")
+public class LoginController {
+	
+	/**
+	 * Metodo para acceder a la vista de logueo de la API mediante las rutas /login o /
+	 * @return
+	 */
+	@GetMapping(value= {"/login", "/"})
+	public ModelAndView login() {
+		// este metodo no regresa un string como en spring mvc debido a que
+		// no tenemos una carpeta WebContent y un dispatcher servlet que nos lleve
+		// a configurar el acceso a ese folder.
+		ModelAndView mav = new ModelAndView();
+		
+		String errorMessage="Sus credenciales pueden estar incorrectas, reviselas";
+		
+		// en spring boot dado que el acceso es a resources/static/templates, no
+		// regresamos un string integrando con Model sino un ModelAndView
+		mav.addObject("errorMsg", errorMessage);
+		mav.setViewName("login"); // acceso a /templates/login.html
+		return mav;
+	}
+	
+}
