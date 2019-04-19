30c21e7ba8ed26de65a2ffb63075bc289a65c5ae
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-19 08:08:40 -0500

    jason viewer project created.

---
+++ IntelliJWorkspace/jasonviewerapi/src/main/java/com/eduardocode/jasonviewerapi/JasonViewerApiApplication.java
@@ -0,0 +1,18 @@
+package com.eduardocode.jasonviewerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1>JasonViewerApiApplication</h1>
+ * Esta
+ * @author Eduardo Rasgado Ruiz
+ */
+@SpringBootApplication
+public class JasonViewerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasonViewerApiApplication.class, args);
+    }
+
+}
