801bfb7e48b235a5bb5943d486a81be70e4ec8e6
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-21 21:49:19 -0500

    Movie virtual object created and documented.

---
+++ IntelliJWorkspace/jasonviewerapi/src/main/java/com/eduardocode/jasonviewerapi/resources/vo/MovieVO.java
@@ -0,0 +1,22 @@
+package com.eduardocode.jasonviewerapi.resources.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * <h1>MovieVO</h1>
+ * Clase para la creacion de objeto virtual de pelicula
+ *
+ * @author Eduardo Rasgado Ruiz
+ * @version 1.0
+ * @since april/2019
+ */
+@Data
+public class MovieVO {
+    private String director;
+    private Integer duration;
+    private Integer timeViewed;
+    private Date startWatching;
+    private Date stopWatching;
+}
