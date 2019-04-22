1ff840bbeab3caa241cc8709674582e0ef216627
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-22 08:26:38 -0500

    Time zone configured in jasonviewerapiApplication class: mexico city time zone.

--- IntelliJWorkspace/jasonviewerapi/src/main/java/com/eduardocode/jasonviewerapi/JasonViewerApiApplication.java
+++ IntelliJWorkspace/jasonviewerapi/src/main/java/com/eduardocode/jasonviewerapi/JasonViewerApiApplication.java
@@ -3,6 +3,9 @@ package com.eduardocode.jasonviewerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 /**
  * <h1>JasonViewerApiApplication</h1>
  * Esta aplicacion permite la transmision de datos de la aplicacion jason viewer desde el servidor hasta el cliente
@@ -18,6 +21,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JasonViewerApiApplication {
 
+    /**
+     * Metodo para configurar la zona horaria que se ocupara en toda la aplicacion
+     */
+    @PostConstruct
+    void started() {
+        // Central Daylight Time	Mexico City
+        TimeZone.setDefault(TimeZone.getTimeZone("CDT"));
+    }
+
+    /**
+     * Puerta de entrada inicial de la aplicacion
+     * @param args argumentos no se utilizan en esta aplicacion
+     */
     public static void main(String[] args) {
 
         SpringApplication.run(JasonViewerApiApplication.class, args);
