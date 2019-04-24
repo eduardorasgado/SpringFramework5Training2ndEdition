a66834f10a5662d15a4faa82799e4244e844da4a
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-04-24 07:13:27 -0500

    login controller created: home and login methods complete. login view updated.

---
+++ IntelliJWorkspace/jasonviewerapi/src/main/java/com/eduardocode/jasonviewerapi/controllers/LoginController.java
@@ -0,0 +1,48 @@
+package com.eduardocode.jasonviewerapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * <h1>LoginController</h1>
+ * Controlador para los request de la sesion del administrador
+ *
+ * @author Eduardo Rasgado Ruiz
+ * @version 1.0
+ * @since april/2019
+ */
+@Controller
+@RequestMapping("/app")
+public class LoginController {
+
+
+    /**
+     * Metodo para la ruta del login
+     * @return retorna una vista a login
+     */
+    @GetMapping({"/login", "/"})
+    public ModelAndView login() {
+        ModelAndView mav = new ModelAndView();
+
+        String errorMessage = "Sus credenciales son incorrectas, compruebe de nuevo.";
+
+        mav.addObject("errorMsg", errorMessage);
+        // templates/login.html
+        mav.setViewName("login");
+        return mav;
+    }
+
+
+    /**
+     * Metodo para la redireccion
+     * @return Una vista a home
+     */
+    @GetMapping("/home")
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("home");
+        return mav;
+    }
+}
