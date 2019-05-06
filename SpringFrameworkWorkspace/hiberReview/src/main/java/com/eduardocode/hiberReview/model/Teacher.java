7248bf1a62b1e819522e94a6fc54b7cf2deb492d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-05 22:02:30 -0500

    Teacher model created and mapped to hibernate

---
+++ SpringFrameworkWorkspace/hiberReview/src/main/java/com/eduardocode/hiberReview/model/Teacher.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.eduardocode.hiberReview.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * <h1>Teacher</h1>
+ * Clase que representa a la entidad Teacher en la base de datos
+ * @author cheetos
+ *
+ */
+@Data
+@Entity
+@Table(name="teacher")
+public class Teacher implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_teacher")
+	private Long idTeacher;
+	private String name;
+	private String avatar;
+	
+	public Teacher(String name, String avatar) {
+		super();
+		this.name = name;
+		this.avatar = avatar;
+	}
+
+	public Teacher() {
+		super();
+	}
+	
+}
