c1c44634a1a5bdfb5d7595353ddb525045115796
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-13 00:44:59 -0500

    Mapping models to hibernate cfg config file. Creating a session to db interaction in app class

--- SpringFrameworkWorkspace/hiberReview/src/main/java/com/eduardocode/hiberReview/App.java
+++ SpringFrameworkWorkspace/hiberReview/src/main/java/com/eduardocode/hiberReview/App.java
@@ -1,5 +1,13 @@
 package com.eduardocode.hiberReview;
 
+import java.util.TimeZone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.eduardocode.hiberReview.model.Teacher;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	TimeZone.setDefault(TimeZone.getTimeZone("America/Mexico_City"));
+    	
+    	// Configuracion de una session para agregar elementos a la db
+        SessionFactory sessionFactory;
+        
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
+        
+        Session session = sessionFactory.openSession();
+        
+        Teacher teacher = new Teacher("Tadeo", "Avatar de Tadeo");
+        
+        // insersion del elemento
+        session.beginTransaction();
+        session.save(teacher);
+        session.getTransaction().commit();
+        session.close();
     }
 }
