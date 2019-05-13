9e760d7914ed3670d2475e959f2f185d16763a68
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-05-13 01:27:19 -0500

    Se crea la interfaz dao para el modelo Teacher.

---
+++ SpringFrameworkWorkspace/hiberReview/src/main/java/com/eduardocode/hiberReview/dao/TeacherDao.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.eduardocode.hiberReview.dao;
+
+import java.util.List;
+
+import com.eduardocode.hiberReview.model.Teacher;
+
+/**
+ * @author cheetos
+ *
+ */
+public interface TeacherDao {
+	
+	/**
+	 * Crea un nuevo teacher y lo agrega a la db
+	 * @param teacher
+	 */
+	public void create(Teacher teacher);
+	
+	/**
+	 * Actualiza un Teacher ya existente en la db
+	 * @param teacher
+	 */
+	void update(Teacher teacher);
+	
+	/**
+	 * Elimina un determinado teacher existente en la db en base a su id
+	 * @param idTeacher
+	 */
+	void delete(Long idTeacher);
+	
+	/**
+	 * Obtiene todos los teachers que existen en la db sin filtro alguno
+	 * @return
+	 */
+	List<Teacher> getAll();
+	
+	/**
+	 * Encuentra y devuelve un teacher en la db dado su id
+	 * @param idTeacher
+	 * @return
+	 */
+	Teacher findById(Long idTeacher);
+	
+	/**
+	 * Encuentra y devuelve un teacher en la db dado su nombre completo
+	 * @param name
+	 * @return
+	 */
+	Teacher findByName(String name);
+
+}
